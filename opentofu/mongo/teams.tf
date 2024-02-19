6d38755bcf237a90e34010b6038795896c3fddd1
sillock1 <jprest97@gmail.com>
2024-02-19 15:36:56 +0000

    fix: matrix filtering for changes and tofu fmt

--- opentofu/mongo/teams.tf
+++ opentofu/mongo/teams.tf
@@ -1,6 +1,6 @@
 resource "mongodbatlas_teams" "team" {
-  for_each   = var.teams
-  org_id     = data.mongodbatlas_roles_org_id.org.org_id
-  name       = each.key
-  usernames  = each.value.users
+  for_each  = var.teams
+  org_id    = data.mongodbatlas_roles_org_id.org.org_id
+  name      = each.key
+  usernames = each.value.users
 }
