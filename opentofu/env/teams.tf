238e6bc17743761fd8284e29c199360529fac21e
sillock1 <jprest97@gmail.com>
2024-02-20 18:48:20 +0000

    formattinggggg

--- opentofu/env/teams.tf
+++ opentofu/env/teams.tf
@@ -1,6 +1,6 @@
 resource "mongodbatlas_teams" "team" {
-  for_each  = {
-    for team in var.atlas_teams:
+  for_each = {
+    for team in var.atlas_teams :
     team.name => team
   }
   org_id    = data.mongodbatlas_roles_org_id.org.org_id
