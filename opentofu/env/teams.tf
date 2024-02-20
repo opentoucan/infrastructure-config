056ac0e08df3766a32448018951ee8b8585d406c
sillock1 <jprest97@gmail.com>
2024-02-20 18:47:28 +0000

    chore: changed map to list of objects because terraform is great at handling them as tf_var envs

--- opentofu/env/teams.tf
+++ opentofu/env/teams.tf
@@ -1,6 +1,9 @@
 resource "mongodbatlas_teams" "team" {
-  for_each  = var.atlas_teams
+  for_each  = {
+    for team in var.atlas_teams:
+    team.name => team
+  }
   org_id    = data.mongodbatlas_roles_org_id.org.org_id
-  name      = each.key
-  usernames = each.value
+  name      = each.value.name
+  usernames = each.value.usernames
 }
