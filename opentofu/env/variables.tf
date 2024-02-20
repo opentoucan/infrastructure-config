5774ecbdbfa3cc96a8dd61512843b09ab02c660c
sillock1 <jprest97@gmail.com>
2024-02-20 18:47:28 +0000

    chore: changed map to list of objects because terraform is great at handling them as tf_var envs

--- opentofu/env/variables.tf
+++ opentofu/env/variables.tf
@@ -1,5 +1,8 @@
 variable "atlas_teams" {
-  type = map(list(string))
+  type = list(object({
+    name      = string
+    usernames = list(string)
+  }))
 
   description = "List of teams with name and email addresses for users"
 }
