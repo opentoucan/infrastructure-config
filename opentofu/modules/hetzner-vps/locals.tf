6f93b57635b6d23f8f4112566f7d5058c84000bb
jared <git@sillock.io>
2025-05-18 13:45:48 +0100

    fix: map and assign primary ip to same server dc

--- opentofu/modules/hetzner-vps/locals.tf
+++ opentofu/modules/hetzner-vps/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  datacentres = { for dc in data.hcloud_datacenters.all.datacenters : dc.location => dc }
+  datacentres = { for dc in data.hcloud_datacenters.all.datacenters : dc.location.name => dc }
 }
--- opentofu/modules/hetzner-vps/primary-ip.tf
+++ opentofu/modules/hetzner-vps/primary-ip.tf
@@ -2,5 +2,6 @@ resource "hcloud_primary_ip" "primary_ip" {
   name          = "primary_ip"
   type          = "ipv4"
   assignee_type = "server"
+  datacenter    = local.datacentres["${var.server_location}"].name
   auto_delete   = false
 }
