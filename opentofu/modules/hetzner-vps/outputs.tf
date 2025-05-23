64dbde745e177c28bdec0025c27743bb1baf6427
sillock <mail@sillock.com>
2025-05-23 17:14:09 +0100

    fix: hetzner vps ip output

--- opentofu/modules/hetzner-vps/outputs.tf
+++ opentofu/modules/hetzner-vps/outputs.tf
@@ -1,5 +1,5 @@
 output "server_ip" {
-  value       = hcloud_primary_ip.primary_ip
+  value       = hcloud_primary_ip.primary_ip.ip_address
   description = "IP address of the server"
   sensitive   = true
 }
