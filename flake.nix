374e34778ea7da3b578f1a6882fe008aa6250437
sillock <mail@sillock.com>
2025-09-12 13:25:02 +0100

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Infrastructure config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  };
+
+  outputs = { nixpkgs, ... } @ inputs:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShells."x86_64-linux".default = with pkgs; mkShell {
+        packages = [
+          opentofu
+          molecule
+          renovate
+          act
+          yamllint
+          ansible
+          atlantis
+        ];
+
+      };
+  };
+}
