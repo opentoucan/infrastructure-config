2d7978687ea36e87694969c488441eb78a92771c
sillock <mail@sillock.com>
2025-10-07 18:15:53 +0100

    fix: into the module we go

--- opentofu/migrations.tf
+++
@@ -1,4 +0,0 @@
-moved {
-  from = module.mongodb.mongodbatlas_cluster.cluster["discord-bot"]
-  to   = module.mongodb.mongodbatlas_advanced_cluster.cluster["discord-bot"]
-}
--- opentofu/modules/atlas-cluster/cluster.tf
+++ opentofu/modules/atlas-cluster/cluster.tf
@@ -3,9 +3,9 @@ resource "mongodbatlas_advanced_cluster" "cluster" {
     for cluster in var.clusters :
     cluster.name => cluster
   }
-  project_id   = mongodbatlas_project.project.id
-  name         = each.value.name
-  cluster_type = each.value.cluster_type
+  project_id     = mongodbatlas_project.project.id
+  name           = each.value.name
+  cluster_type   = each.value.cluster_type
   backup_enabled = each.value.backup_enabled
 
   replication_specs = [
@@ -15,8 +15,11 @@ resource "mongodbatlas_advanced_cluster" "cluster" {
           provider_name         = each.value.provider_name
           backing_provider_name = each.value.backing_provider_name
           region_name           = each.value.region_name
-          instance_size         = each.value.instance_size
           priority              = each.value.priority
+          electable_specs = {
+            instance_size = each.value.instance_size
+            node_count    = 3
+          }
         }
       ]
     }
