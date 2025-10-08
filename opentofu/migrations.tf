dd73c356efe4c5cec7237abdef929be502b678aa
sillock <mail@sillock.com>
2025-10-08 10:48:23 +0100

    fix(opentofu): explicit project reference for atlas

--- opentofu/migrations.tf
+++ opentofu/migrations.tf
@@ -2,5 +2,5 @@ import {
   for_each = local.mongodbatlas_teams_with_ids
 
   to       = module.mongodb.mongodbatlas_team_project_assignment.team_project_assignment[each.key]
-  id       = "${module.mongodb.mongodbatlas_project.project.id}/${each.key}"
+  id       = "65d4a3bf160f504f2547c2cd/${each.key}"
 }
