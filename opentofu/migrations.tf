3b92d7404767712cb33176416dda49307a17fc42
sillock <mail@sillock.com>
2025-10-08 10:16:07 +0100

    fix(opentofu): re-import cluster as atlas advanced cluster

--- opentofu/migrations.tf
+++ opentofu/migrations.tf
@@ -1,7 +1,4 @@
-removed {
-  from = module.mongodb.mongodbatlas_cluster.cluster
-
-  lifecycle {
-    destroy = false
-  }
+import {
+  to = module.mongodb.mongodbatlas_advanced_cluster.cluster["discord-bot"]
+  id = "65d4a3bf160f504f2547c2cd-discord-bot"
 }
--- opentofu/modules/atlas-cluster/cluster.tf
+++ opentofu/modules/atlas-cluster/cluster.tf
@@ -1,27 +1,27 @@
-# resource "mongodbatlas_advanced_cluster" "cluster" {
-#   for_each = {
-#     for cluster in var.clusters :
-#     cluster.name => cluster
-#   }
-#   project_id     = mongodbatlas_project.project.id
-#   name           = each.value.name
-#   cluster_type   = each.value.cluster_type
-#   backup_enabled = each.value.backup_enabled
+resource "mongodbatlas_advanced_cluster" "cluster" {
+  for_each = {
+    for cluster in var.clusters :
+    cluster.name => cluster
+  }
+  project_id     = mongodbatlas_project.project.id
+  name           = each.value.name
+  cluster_type   = each.value.cluster_type
+  backup_enabled = each.value.backup_enabled
 
-#   replication_specs = [
-#     {
-#       region_configs = [
-#         {
-#           provider_name         = each.value.provider_name
-#           backing_provider_name = each.value.backing_provider_name
-#           region_name           = each.value.region_name
-#           priority              = each.value.priority
-#           electable_specs = {
-#             instance_size = each.value.instance_size
-#             node_count    = 1
-#           }
-#         }
-#       ]
-#     }
-#   ]
-# }
+  replication_specs = [
+    {
+      region_configs = [
+        {
+          provider_name         = each.value.provider_name
+          backing_provider_name = each.value.backing_provider_name
+          region_name           = each.value.region_name
+          priority              = each.value.priority
+          electable_specs = {
+            instance_size = each.value.instance_size
+            node_count    = 1
+          }
+        }
+      ]
+    }
+  ]
+}
